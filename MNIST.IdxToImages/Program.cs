63c9c406c8c9f26a92c88006eb1eb43921c9da66
Nikola Petrovic <nikola.petrovic.1999@gmail.com>
2021-08-23 00:20:37 +0200

    Working with Format8bppIndexed.

--- MNIST.IdxToImages/Program.cs
+++ MNIST.IdxToImages/Program.cs
@@ -2,7 +2,11 @@
 using Microsoft.Extensions.Logging;
 using Np.Imaging;
 using System;
+using System.Collections.Generic;
+using System.Drawing;
 using System.IO;
+using System.Runtime.InteropServices;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace MNIST.IdxToImages
@@ -14,10 +18,13 @@ namespace MNIST.IdxToImages
         public string ImagesOutputPath { get; }
         public bool SplitImagesByLabel { get; }
         public string ImageExtension { get; }
+        public int MaxImagesToExport { get; }
 
-        protected FileStream imagesFileStream;
+        protected System.Drawing.Imaging.ImageFormat ImageFormat;
 
-        protected FileStream labelsFileStream;
+        protected FileStream ImagesFileStream;
+
+        protected FileStream LabelsFileStream;
 
         protected ILogger<IdxExporter> logger;
 
@@ -25,7 +32,8 @@ namespace MNIST.IdxToImages
                      string labelsFilePath,
                      string imagesOutputPath,
                      bool splitImagesByLabel,
-                     string imageExtension)
+                     string imageExtension,
+                     int maxImagesToExport)
         {
             logger = Program.LoggerFactory.CreateLogger<IdxExporter>();
 
@@ -48,7 +56,7 @@ namespace MNIST.IdxToImages
             {
                 throw new ArgumentException($"'{nameof(imageExtension)}' cannot be null or whitespace.", nameof(imageExtension));
             }
-            if (!Image.Extension.IsValidExtension(imageExtension))
+            if (!Np.Imaging.Image.Extension.IsValidExtension(imageExtension))
             {
                 throw new ArgumentException($"'{nameof(imageExtension)}' is an invalid image extension.", nameof(imageExtension));
             }
@@ -58,61 +66,166 @@ namespace MNIST.IdxToImages
             ImagesOutputPath = imagesOutputPath;
             SplitImagesByLabel = splitImagesByLabel;
             ImageExtension = imageExtension;
-
-            logger.LogInformation($"Opening {imagesFilePath}");
-            imagesFileStream = File.OpenRead(imagesFilePath);
-            logger.LogInformation($"Opening {labelsFilePath}");
-            labelsFileStream = File.OpenRead(labelsFilePath);
+            MaxImagesToExport = maxImagesToExport;
+            ImageFormat = imageExtension.ToLower() switch
+            {
+                ".png" => System.Drawing.Imaging.ImageFormat.Png,
+                ".jpg" or ".jpeg" => System.Drawing.Imaging.ImageFormat.Jpeg,
+                ".tif" or ".tiff" => System.Drawing.Imaging.ImageFormat.Tiff,
+                ".bmp" => System.Drawing.Imaging.ImageFormat.Bmp,
+                ".gif" => System.Drawing.Imaging.ImageFormat.Gif,
+                _ => throw new ArgumentException($"'{nameof(ImageExtension)}' is unsupported."),
+            };
+            logger.LogInformation($"Opening {imagesFilePath}.");
+            ImagesFileStream = File.OpenRead(imagesFilePath);
+            logger.LogInformation($"Opening {labelsFilePath}.");
+            LabelsFileStream = File.OpenRead(labelsFilePath);
         }
 
         public void Dispose()
         {
-            imagesFileStream?.Dispose();
-            labelsFileStream?.Dispose();
+            ImagesFileStream?.Dispose();
+            LabelsFileStream?.Dispose();
         }
 
         public async Task Export()
         {
-            int FlipByteOrder(int i)
-            {
-                unsafe
-                {
-                    int* ptr = &i;
-                    int bytesToFlip = sizeof(int)
-                }
-                
-            }
-
             // Preprocess Image File
-            byte[] imageInfo = new byte[2 * 4 * sizeof(int)];
-            int imageOffset = 0;
+            byte[] imageInfo = new byte[sizeof(int)];
 
-            await imagesFileStream.ReadAsync(imageInfo, imageOffset += sizeof(int), sizeof(int));
-            await imagesFileStream.ReadAsync(imageInfo, imageOffset += sizeof(int), sizeof(int));
-            await imagesFileStream.ReadAsync(imageInfo, imageOffset += sizeof(int), sizeof(int));
-            await imagesFileStream.ReadAsync(imageInfo, imageOffset += sizeof(int), sizeof(int));
+            await ImagesFileStream.ReadAsync(imageInfo, 0, sizeof(int));
+            Array.Reverse(imageInfo);
+            int imageMagicNumber = BitConverter.ToInt32(imageInfo, 0);
 
-            imageOffset = 0;
-            int imageMagicNumber = FlipByteOrder(BitConverter.ToInt32(imageInfo, imageOffset += sizeof(int)));
-            int numberOfImages = BitConverter.ToInt32(imageInfo, imageOffset += sizeof(int));
-            int imageNumberOfRows = BitConverter.ToInt32(imageInfo, imageOffset += sizeof(int));
-            int imageNumberOfColumns = BitConverter.ToInt32(imageInfo, imageOffset += sizeof(int));
+            await ImagesFileStream.ReadAsync(imageInfo, 0, sizeof(int));
+            Array.Reverse(imageInfo);
+            int numberOfImages = BitConverter.ToInt32(imageInfo, 0);
+
+            await ImagesFileStream.ReadAsync(imageInfo, 0, sizeof(int));
+            Array.Reverse(imageInfo);
+            int imageNumberOfRows = BitConverter.ToInt32(imageInfo, 0);
+
+            await ImagesFileStream.ReadAsync(imageInfo, 0, sizeof(int));
+            Array.Reverse(imageInfo);
+            int imageNumberOfColumns = BitConverter.ToInt32(imageInfo, 0);
 
             // Preprocess Label File
 
-            byte[] labelInfo = new byte[2 * 2 * sizeof(int)];
-            int labelOffset = 0;
+            byte[] labelInfo = new byte[sizeof(int)];
 
-            await labelsFileStream.ReadAsync(labelInfo, labelOffset += sizeof(int), sizeof(int));
-            await labelsFileStream.ReadAsync(labelInfo, labelOffset += sizeof(int), sizeof(int));
+            await LabelsFileStream.ReadAsync(labelInfo, 0, sizeof(int));
+            Array.Reverse(labelInfo);
+            int labelMagicNumber = BitConverter.ToInt32(labelInfo, 0);
 
-            labelOffset = 0;
-            int labelMagicNumber = BitConverter.ToInt32(labelInfo, labelOffset += sizeof(int));
-            int numberOfLabels = BitConverter.ToInt32(labelInfo, labelOffset += sizeof(int));
+            await LabelsFileStream.ReadAsync(labelInfo, 0, sizeof(int));
+            Array.Reverse(labelInfo);
+            int numberOfLabels = BitConverter.ToInt32(labelInfo, 0);
+
+            // Validate
+
+            ValidateParsedParams(imageMagicNumber, numberOfImages, labelMagicNumber, numberOfLabels);
 
             // Exporting
 
+            DirectoryInfo outDirInfo = EnsureOutputDirectory();
 
+            // Process streams
+
+            int maxDigits = (int)Math.Ceiling(Math.Log10(numberOfImages));
+            Dictionary<string, int> labels = new();
+
+            byte[] labelBytes = new byte[1];
+            byte[] pixels = new byte[imageNumberOfRows * imageNumberOfColumns];
+
+            int min = Math.Min(Math.Min(numberOfImages, numberOfLabels), MaxImagesToExport);
+            for (int i = 0; i < min; i++)
+            {
+                await ImagesFileStream.ReadAsync(pixels);
+                await LabelsFileStream.ReadAsync(labelBytes);
+
+                string label = $"{labelBytes[0]}";
+
+                if (!labels.ContainsKey(label))
+                {
+                    labels.Add(label, 0);
+                    if (SplitImagesByLabel)
+                        outDirInfo.CreateSubdirectory(label);
+                }
+                else
+                    labels[label]++;
+
+                string fileName = $"{label}" +
+                    $"-{i.ToString($"D{maxDigits}")}" +
+                    $"-{labels[label].ToString($"D{maxDigits}")}" +
+                    $"{ImageExtension}";
+                string filePath;
+
+                if (SplitImagesByLabel)
+                    filePath = Path.Join(outDirInfo.FullName, label, fileName);
+                else
+                    filePath = Path.Join(outDirInfo.FullName, fileName);
+
+                Bitmap bitmap = new Bitmap(imageNumberOfColumns,
+                                                        imageNumberOfRows,
+                                                        System.Drawing.Imaging.PixelFormat.Format8bppIndexed);
+
+                unsafe
+                {
+                    System.Drawing.Imaging.BitmapData bitmapData = bitmap.LockBits(
+                    new Rectangle(
+                        0,
+                        0,
+                        imageNumberOfRows,
+                        imageNumberOfColumns),
+                    System.Drawing.Imaging.ImageLockMode.ReadWrite,
+                    System.Drawing.Imaging.PixelFormat.Format8bppIndexed);
+
+                    int stride = bitmapData.Stride;
+
+                    byte* scan0 = (byte*)bitmapData.Scan0.ToPointer();
+                    IntPtr scan0Ptr = (IntPtr)scan0;
+                    for (int j = 0; j < pixels.Length; j++)
+                    {
+                        Marshal.WriteByte(scan0Ptr + j, pixels[j]);
+                    }
+
+                    bitmap.UnlockBits(bitmapData);
+                }
+                System.Drawing.Image image = bitmap;
+
+                logger.LogInformation($"Saving image '{fileName}'.");
+                image.Save(filePath, ImageFormat);
+            }
+
+            logger.LogInformation($"Finished exporting {min} images.");
+        }
+
+        private DirectoryInfo EnsureOutputDirectory()
+        {
+            DirectoryInfo outDirInfo;
+            try
+            {
+                outDirInfo = Directory.CreateDirectory(ImagesOutputPath);
+            }
+            catch (Exception)
+            {
+                logger.LogError($"Failed to create '{nameof(ImagesOutputPath)}' with value '{ImagesOutputPath}'.");
+                throw;
+            }
+
+            return outDirInfo;
+        }
+
+        private void ValidateParsedParams(int imageMagicNumber, int numberOfImages, int labelMagicNumber, int numberOfLabels)
+        {
+            if (imageMagicNumber != 2051)
+                logger.LogWarning($"'{nameof(imageMagicNumber)}' is invalid.");
+
+            if (labelMagicNumber != 2049)
+                logger.LogWarning($"'{nameof(labelMagicNumber)}' is invalid.");
+
+            if (numberOfImages != numberOfLabels)
+                logger.LogWarning($"'{nameof(numberOfImages)}' doesn't match '{nameof(numberOfLabels)}'");
         }
     }
 
@@ -131,7 +244,7 @@ namespace MNIST.IdxToImages
             public string IdxLabelsFilePath { get; set; }
 
             [Option('s', "split", Required = false,
-                Default = true,
+                Default = false,
                 HelpText = "Whether to split the images into subfolders by the label value")]
             public bool IdxImagesSplitByValue { get; set; }
 
@@ -144,6 +257,11 @@ namespace MNIST.IdxToImages
                 Default = ".png",
                 HelpText = "The extension of the output images")]
             public string IdxImagesExtension { get; set; }
+
+            [Option('n', "image-number", Required = false,
+                Default = 100,
+                HelpText = "Maxmimum number of images to export")]
+            public int IdxMaxImagesToExport { get; internal set; }
         }
 
         public static IdxExporter IdxExporter { get; set; }
@@ -163,12 +281,15 @@ namespace MNIST.IdxToImages
             Parser.Default.ParseArguments<Options>(args)
                 .WithParsed(o =>
                 {
-                    IdxExporter = new IdxExporter(o.IdxImagesFilePath,
+                    using (IdxExporter = new IdxExporter(o.IdxImagesFilePath,
                         o.IdxLabelsFilePath,
                         o.IdxImagesOutputPath,
                         o.IdxImagesSplitByValue,
-                        o.IdxImagesExtension);
-                    IdxExporter.Export().GetAwaiter().GetResult();
+                        o.IdxImagesExtension,
+                        o.IdxMaxImagesToExport))
+                    {
+                        IdxExporter.Export().GetAwaiter().GetResult();
+                    }
                 });
         }
     }
