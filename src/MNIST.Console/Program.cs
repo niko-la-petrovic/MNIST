54e181ed66ac22a886a2d327edecbc26d0c1f7ea
Nikola Petrovic <nikola.petrovic.1999@gmail.com>
2021-08-23 09:51:19 +0200

    Changed project structure. Fixed dependency issue.

---
+++ src/MNIST.Console/Program.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.ML;
+using Microsoft.ML.Data;
+using System;
+
+namespace MNIST.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
